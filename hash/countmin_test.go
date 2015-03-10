0166a7845cf162e09cc9373f407ad6a63245b2d4
Lars Buitinck <l.buitinck@esciencecenter.nl>
2015-03-10 15:07:43 +0100

    add count-min sketch

---
+++ hash/countmin_test.go
@@ -0,0 +1,26 @@
+package hash
+
+import (
+    "math"
+    "math/rand"
+    "testing"
+)
+
+func TestCountMin(t *testing.T) {
+    sketch := New(210, 1300)
+    freq := make(map[uint32]uint32)
+
+    rng := rand.New(rand.NewSource(42))
+    for i := 0; i < 10000; i++ {
+        h := rng.Uint32()
+        sketch.Add(h, 1)
+        freq[h] += 1
+    }
+
+    // XXX Should test if error is within margin with some probability.
+    for k, v := range freq {
+        if math.Abs(float64(sketch.Get(k) - v)) > 4 {
+            t.Errorf("difference too big: got %d, want %d", sketch.Get(k), v)
+        }
+    }
+}
