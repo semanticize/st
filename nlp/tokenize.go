ae192ddddca22bd552f1c09258d13da76ee2866c
Lars Buitinck <l.buitinck@esciencecenter.nl>
2015-04-19 14:59:51 +0200

    micro-optimize Tokenize

--- nlp/tokenize.go
+++ nlp/tokenize.go
@@ -10,13 +10,14 @@ var (
 	tokenRE   = regexp.MustCompile(`(\w|\b['\.,]\b)+`)
 )
 
-func Tokenize(s string) []string {
-	out := make([]string, 0)
-	for _, token := range tokenRE.FindAllString(s, -1) {
+func Tokenize(s string) (tokens []string) {
+	matches := tokenRE.FindAllString(s, -1)
+	tokens = make([]string, 0, len(matches))
+	for _, token := range matches {
 		if numericRE.MatchString(token) {
 			token = "<NUM>"
 		}
-		out = append(out, token)
+		tokens = append(tokens, token)
 	}
-	return out
+	return
 }
