6992ff01f0af1a1e155f8c5de19d4053b6c09ba5
Lars Buitinck <l.buitinck@esciencecenter.nl>
2015-11-03 18:22:50 +0100

    speed up n-gram generation by ~1/3

--- nlp/ngrams.go
+++ nlp/ngrams.go
@@ -15,21 +15,20 @@ func HashNGram(h hash.Hash32, tokens []string) uint32 {
 
 // Generate n-grams of length in [minN, maxN].
 func NGrams(tokens []string, minN, maxN int) [][]string {
-	out := make([][]string, 0)
-	for i := range tokens {
-		for n := minN; n <= min(maxN, len(tokens)-i); n++ {
-			out = append(out, tokens[i:i+n])
-		}
+	positions := NGramsPos(tokens, minN, maxN)
+	out := make([][]string, 0, len(positions))
+	for _, pos := range positions {
+		out = append(out, tokens[pos[0]:pos[1]])
 	}
 	return out
 }
 
 // Generate start/end positions of n-grams of length in [minN, maxN].
 func NGramsPos(tokens []string, minN, maxN int) [][2]int {
-	out := make([][2]int, 0)
+	out := make([][2]int, 0, (maxN-minN+1)*len(tokens))
 	for i := range tokens {
 		for n := minN; n <= min(maxN, len(tokens)-i); n++ {
-			out = append(out, [2]int{i, i+n})
+			out = append(out, [2]int{i, i + n})
 		}
 	}
 	return out
