5a65e8f21ac6d73aea222917214062b12cd8b675
Lars Buitinck <l.buitinck@esciencecenter.nl>
2015-11-03 16:34:31 +0100

    new function to hash an n-gram

--- nlp/ngrams.go
+++ nlp/ngrams.go
@@ -1,5 +1,18 @@
 package nlp
 
+import (
+	"hash"
+	"io"
+)
+
+func HashNGram(h hash.Hash32, tokens []string) uint32 {
+	for _, token := range tokens {
+		io.WriteString(h, token)
+		h.Write([]byte("\x00"))
+	}
+	return h.Sum32()
+}
+
 // Generate n-grams.
 func NGrams(tokens []string, minN, maxN int) [][]string {
 	out := make([][]string, 0)
